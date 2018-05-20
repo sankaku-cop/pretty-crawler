ce5660246e80b76dff277f79c1a7f5b534f3d7b6
COP <drum_can_studio@yahoo.co.jp>
2018-05-20 18:46:43 +0900

    crawler実装

---
+++ PrettyCrawler/PrettyCrawler/Crawler/Prichan/DetailUrlExtractor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using HtmlAgilityPack;
+
+namespace PrettyCrawler.Crawler.Prichan
+{
+    public class DetailUrlExtractor
+    {
+        /// <summary>
+        /// 引数のページに紐づくチケット詳細Urlの一覧を返す
+        /// </summary>
+        /// <param name="htmlText">root html text</param>
+        /// <returns>detail urls</returns>
+        public IEnumerable<string> Extract(string htmlText)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(htmlText);
+
+            return doc.DocumentNode.SelectNodes(@"//a")
+                .Select(node => node.GetAttributeValue("href", string.Empty))
+                .Where(url => url.StartsWith("details/"));
+        }
+    }
+}
